b85a7278462a27e841933e587bb013d52cdc1345
saalimon <niti08@gmail.com>
2019-10-30 20:43:17 +0700

    remove pyc

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 from flask_restful import Resource, Api
-import numpy as np
-import pandas as pd
 import sys
 sys.path.insert(0, './handler')
 from handler import Data, Upload
-
 app = Flask(__name__)
 api = Api(app)
-#root for test upload
 @app.route('/')
 def home():
     return render_template('index.html')
