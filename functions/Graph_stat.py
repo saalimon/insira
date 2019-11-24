870ccc59fd2ce94a390502c6ccb9198e4e062218
Nz-zero <nattapatyuvasuta@gmail.com>
2019-11-24 21:41:05 +0700

    numercial column skewness beta

---
+++ functions/Graph_stat.py
@@ -0,0 +1,26 @@
+def numercial_data_distribution(df, df_type):
+    '''This function recieve original dataframe & data type and output a
+    dataframe of graph skewness of each numerical column in original dataframe'''
+    
+    column_skew = []
+    
+    data_send_filter_num = df_type.loc[(df_type.col_type == 'numeric')]
+    num_col = [col for col in data_send_filter_num['col_name']]
+    
+    for col in num_col:
+        mean = df[col].mean()
+        med = df[col].median()
+
+        if math.isclose(mean, med, rel_tol=0.1e-1):
+            column_skew.append('Symmetric')
+        elif mean > med:
+            column_skew.append('Right skewed')
+        elif med > mean:
+            column_skew.append('Left skewed')
+        else:
+            column_skew.append('fail to process')
+            
+    skew_list = {'col_name':num_col, 'skewness':column_skew}
+    df_send = pd.DataFrame(skew_list) 
+    
+    return df_send
