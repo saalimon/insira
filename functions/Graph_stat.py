ff356a415910244fd776cb30ca0852ae2a623a63
Nz-zero <nattapatyuvasuta@gmail.com>
2019-12-04 17:41:34 +0700

    add dominated_category function for detect dominate value

--- functions/Graph_stat.py
+++ functions/Graph_stat.py
@@ -28,3 +28,59 @@ def numercial_data_distribution(df, df_type):
     df_send = pd.DataFrame(skew_list) 
     
     return df_send
+
+def dominated_category(cat_count):
+    '''This function recieve a dictionary contained count value 
+    of the category type column(from function cat_data_count.py) and
+    return a dataframe of the dominated attribute of each column if existed'''
+    
+    #init list
+    col_list, anomal, attribute, value, percent_dominate = ([] for i in range(5))
+    
+    #loop each column
+    for col in cat_count.keys():
+        col_list.append(col)
+        
+        #list for holding value of current column
+        temp_value = []
+        
+        #append each value in each row
+        for index,row in cat_count[col].iterrows():
+            temp_value.append(row.item())
+        
+        #for checking if it is the dominate one in dataframe
+        member = len(temp_value)         
+        max_value = np.max(temp_value)
+        threshold = 0
+        
+        for i in temp_value:
+            if i != max_value:
+                
+                #if value of current attribute is lower than 35% when compared to maximum value existed in column
+                if i/max_value < 0.35:
+                    #increase the number of normal value compared to anomaly
+                    threshold += 1
+                    
+            #for store max value purpose
+            else:
+                max_attribute = index
+        
+        #if overall nomal value is equal to number of attribute exclude max value then that max value is certainly an anomaly
+        if threshold == member-1:
+            anomal.append(1)
+            attribute.append(max_attribute)
+            value.append(max_value)
+            percent_dominate.append(max_value/sum(temp_value))
+    #         print(str(max_col)+" in "+str(col)+" is anomalies")
+        else:
+            anomal.append(0)
+            attribute.append(None)
+            value.append(None)
+            percent_dominate.append(None)
+    #         print("No abnormalies is detected")
+
+    abnormalies_list = {'col_name':col_list, 'anomaly_detected':anomal, 'attribute':attribute,
+                        'value':value, 'percent_dominate': percent_dominate}
+    df_anomaly = pd.DataFrame(abnormalies_list) 
+
+    return df_anomaly
