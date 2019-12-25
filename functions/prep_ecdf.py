cb468599ea9c82746a1a9e7fc7678739e6fac3a6
Nz-zero <nattapatyuvasuta@gmail.com>
2019-12-25 22:23:05 +0700

    fix ecdf x and y axis to dataframe

--- functions/prep_ecdf.py
+++ functions/prep_ecdf.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
 
-def prep_ecdf(df):
+def prep_ecdf(data):
     ''' function to plot ecdf taking a column of data as input
     and return axis & yaxis for ploting graph. Also return percentage & 
-    value of data & argument for use in NLG and score 
-    that use for selecting graph
+    value of data & argument for use in NLG and
+    score for selecting graph
     Recommended graph label: title = "ECDF Plot", xlabel = 'Data Values', ylabel = 'Percentage'
     '''
     
     temp = 0
-    data_length = len(df)
-    xaxis = np.sort(df)
+    data_length = len(data)
+    xaxis = np.sort(data)
     yaxis = np.arange(1, data_length+1)/data_length
     
     for i,v in enumerate(xaxis):
@@ -22,5 +22,8 @@ def prep_ecdf(df):
                 else:
                      return (1, xaxis, yaxis, (1-i/data_length)*100, v, 'more_than')
         temp = v
+        
+    ecdf_axis = {'xaxis':xaxis, 'yaxis':yaxis}
+    df_ecdf = pd.DataFrame(ecdf_axis) 
     
-    return (0, xaxis, yaxis, 50, xaxis[int(data_length/2)], 'less_than')
+    return (0, df_ecdf, 50, xaxis[int(data_length/2)], 'less_than')
